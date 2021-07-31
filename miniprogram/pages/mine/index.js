78c124f743c15a8dfd31550ecd8221aaf8bae5b9
1328497946 <1328497946@qq.com>
2021-07-31 10:01:43 +0800

    page 我 登录处理

--- miniprogram/pages/mine/index.js
+++ miniprogram/pages/mine/index.js
@@ -1,54 +1,91 @@
 // pages/auth/index.js
 const util = require('../../utils/utils.js');
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo: {},
-    isLogin: false
+    userInfo: null,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+  setProfile() {
+    wx.getUserProfile({
+      desc: '授权用户信息，用于登录',
+      success(data) {
+        app.globalData.userInfo = data.userInfo;
+        const item = {
+          userInfo: data.userInfo,
+          expires_time: (new Date()).getTime()
+        };
+        wx.setStorageSync('app_data', item);
+        // console.log(data.userInfo);
+      },
+      fail() {
+        // console.log(data.errMsg);
+      }
+    })
+  },
   onClickLogin: util.throttle(function(event) {
     let that = this;
-    if(!this.data.isLogin) {
-      wx.getSetting({
-        success(res) {
-          if(res.authSetting['scope.userInfo']) {
-            wx.getUserProfile({
-              desc: 'desc',
-              success(data) {
-                console.log(data.userInfo);
-                that.setData({
-                  userInfo: data.userInfo,
-                  isLogin: true
-                })
-              },
-              fail() {
-                wx.showToast({
-                  title: '取消授权',
-                  icon: 'none',
-                  duration: 1500
-                })
-              }
-            })
-          }
-        }
-      });
+    const { userInfo } = app.globalData;
+    if(userInfo) {
+      // console.log("已经有信息");
+      this.setData({
+        userInfo
+      })
+    } else {
+      // 获取登录信息
+      that.setProfile();
+      that.checkRegister();
     }
+    console.log(app.globalData.userInfo)
+    this.setData({
+      userInfo: app.globalData.userInfo
+    })
   }),
   onLoad: function() {
-    
+    let that = this;
+    const { userInfo } = app.globalData;
+    // console.log(111111, userInfo);
+    if(userInfo) {
+      // 已经有信息
+      that.checkRegister(userInfo);
+      that.setData({
+        userInfo
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
+  checkRegister: function(userInfo) {
+    wx.cloud.callFunction({
+      name: 'get_setUserInfo',
+      data: {
+        getSelf: true
+      }
+    }).then((res) => {
+      // console.log(res.result);
+      if(!res.result?.data) {
+        wx.cloud.callFunction({
+          name: 'get_setUserInfo',
+          data: {
+            setSelf: true,
+            userData: userInfo
+          }
+        })
+      } else {
+        // console.log("已经注册")
+      }
+    })
+  },
   onReady: function () {
-
+    
   },
 
   /**
